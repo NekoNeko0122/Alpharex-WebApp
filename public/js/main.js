259e749f4ae67ec9dede6695fef1c172e5f18e56
NekoNeko0122 <166288191+NekoNeko0122@users.noreply.github.com>
2025-03-13 20:14:07 +0800

    remove touch

--- public/js/main.js
+++ public/js/main.js
@@ -14,6 +14,7 @@ var hammer = new Hammer(document.documentElement);
 
 var swiper = new Swiper(".swiper", {
     effect: "cube",
+    allowTouchMove: false,
     grabCursor: false,
     cubeEffect: {
         shadow: true,
