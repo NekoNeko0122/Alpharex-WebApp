e050bb89dc249a4f630b826235ea6448e1001e5f
NekoNeko0122 <166288191+NekoNeko0122@users.noreply.github.com>
2025-03-24 17:53:08 +0800

    try

--- public/js/main.js
+++ public/js/main.js
@@ -180,22 +180,3 @@ function adjustHeight() {
   window.addEventListener('resize', adjustHeight);
 
 
-const isIpadPro11 = window.matchMedia("(min-width: 834px) and (max-width: 1194px)").matches;
-
-// Detect iPad Pro 12.9-inch using matchMedia
-const isIpadPro12 = window.matchMedia("(min-width: 1024px) and (max-width: 1366px)").matches;
-
-
-if (isIpadPro11) {
-  console.log("iPad Pro 11-inch detected!");
-  // Run JS code specific to iPad Pro 11
-  guessBtn.textContent = "IPAD PRO 11";
-  servBtn.textContent = "IPAD PRO 11"
-} else if (isIpadPro12) {
-  console.log("iPad Pro 12.9-inch detected!");
-  guessBtn.textContent = "IPAD PRO 12";
-  servBtn.textContent = "IPAD PRO 12"
-  
-} else {
-  console.log("Not iPad Pro!");
-}
